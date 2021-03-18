34c7a72045d87508ac64b50e66990965dc1a4688
Nyjia Scott <nkscott97@gmail.com>
2021-03-18 13:45:21 -0400

    Assignments Update 3

--- Module-2/HTTP/Axios-Todo-Project/index.js
+++ Module-2/HTTP/Axios-Todo-Project/index.js
@@ -1,6 +1,7 @@
 const todoForm = document.getElementById("todo")
 const doneBtn = document.getElementById("submit")
 const todoList = document.getElementById("todo-list")
+let editing = false
 
 function getTodo() {
 axios.get("https://api.vschool.io/nyjiascott/todo")
@@ -14,7 +15,7 @@ function listTodo(data) {
     console.log("Is listTodo working?")
 
     for(let i = 0; i < data.length; i++) {
-        const listItems = document.createElement("li")
+        const listItem = document.createElement("li")
         const title = document.createElement("h2")
         const description = document.createElement("p")
         const price = document.createElement("p")
@@ -23,7 +24,7 @@ function listTodo(data) {
         const deleteBtn = document.createElement("button")
         const editBtn = document.createElement("button")
 
-        listItems.classList.add("list-items")
+        listItem.classList.add("list-items" + data[i]._id)
         title.textContent = data[i].title
         title.classList.add("title")
         description.textContent = data[i].description
@@ -37,8 +38,8 @@ function listTodo(data) {
         deleteBtn.textContent = "Delete"
         deleteBtn.classList.add("delete-btn")
         editBtn.textContent = "Edit"
-        editBtn.classList.add("edit-btn")
-
+        editBtn.classList.add("edit-btn" + data[i]._id)
+        
         if(data[i].completed === true) {
             title.style.textDecoration = "line-through"
             checkBox.checked = true
@@ -51,6 +52,59 @@ function listTodo(data) {
             getTodo()
         })
 
+        editBtn.addEventListener("click", function() {
+            if(editing === false) {
+                let querySelectedListItem = document.querySelector(".list-Item" + data[i]._id)
+
+                let changeTitle = document.createElement("input")
+                changeTitle.defaultValue = data[i].title
+                changeTitle.classList.add("title" + data[i]._id); //Added unique classnams to inputs using id's to access later
+                let changeDescription =  document.createElement("input")
+                changeDescription.defaultValue = data[i].description
+                changeDescription.classList.add("description" + data[i]._id);
+                let changePrice = document.createElement("input")
+                changePrice.classList.add("price" + data[i]._id);
+                changePrice.defaultValue = data[i].price
+                let changeImage = document.createElement("input")
+                changeImage.classList.add("image" + data[i]._id);
+                changeImage.defaultValue = data[i].imgUrl
+                let querySelectedEditBtn = document.querySelector(".edit-Btn" + data[i]._id)
+                querySelectedListItem.append(changeTitle)
+                querySelectedListItem.append(changeDescription)
+                querySelectedListItem.append(changePrice)
+                querySelectedListItem.append(changeImage)
+                querySelectedEditBtn.textContent = "submit"
+            }
+                if(editing === true) {
+                    let querySelectedChangeTitle = document.querySelector(".title" + data[i]._id) //It didn't seem like JS knew which inputs I was talking about unless I did this querySelector overkill. I think the block scopes of these if statements have to do with that.  
+                    let querySelectedChangeDescription = document.querySelector(".description" + data[i]._id)
+                    let querySelectedChangePrice = document.querySelector(".price" + data[i]._id)
+                    let querySelectedChangeImage = document.querySelector(".image" + data[i]._id)
+                    let querySelectedEditBtn = document.querySelector(".edit-Btn" + data[i]._id) //did querySelector overkill with the edit button as JS also didn't know what I was talking about once I got into this if statement
+                    let editItems = {
+                        title: querySelectedChangeTitle.value,
+                        description: querySelectedChangeDescription.value,
+                        price: querySelectedChangePrice.value,
+                        imgUrl: querySelectedChangeImage.value
+                    }
+                
+                    axios.put("https://api.vschool.io/nyjiascott/todo/" + data[i]._id, editItems)
+                        .then(response => getTodo(response.data)) //auto-update page after put request
+                        .catch(error => console.log(error))
+                    querySelectedChangeTitle.remove()
+                    querySelectedChangeDescription.remove()
+                    querySelectedChangePrice.remove()
+                    querySelectedChangeImage.remove()
+                    querySelectedEditBtn.textContent = "edit"
+            }
+            editing = !editing //This exclamation point means to make whatever comes after it the oppposite of what it is. This statement is saying editing will noq equal of what editing was previously
+            return
+        })
+
+        deleteBtn.textContent = "delete"
+        todoList.append(listItem)
+        listItem.append(title, description, price, image, checkBox, deleteBtn, editBtn)
+   
         deleteBtn.addEventListener("click", function(event) {
             event.preventDefault()
 
@@ -99,7 +153,7 @@ function listTodo(data) {
 
             // const saveEditBtn = document.createElement("button")
             // saveEditBtn.textContent = "save"
-            // listItems.append(saveEditBtn)
+            // listItem.append(saveEditBtn)
 
             // saveEditBtn.addEventListener("submit", function(event) {
             //     event.preventDefault() 
@@ -119,8 +173,10 @@ function listTodo(data) {
             //     console.log("Is editBtn working?")
         // })
 
-            todoList.append(listItems)
-            listItems.append(title, description, price, image, editBtn, deleteBtn)
+        
+
+            todoList.append(listItem)
+            listItem.append(title, description, price, image, editBtn, deleteBtn)
             title.prepend(checkBox)
     
             console.log("Is for loop working?")
@@ -150,7 +206,7 @@ function listTodo(data) {
                 todoForm.photo.value = ""
 
                 axios.post("https://api.vschool.io/nyjiascott/todo", newItems)
-                    .then(response => getTodo())
+                    .then(response => getTodo(response.data))
                     .catch(error => console.log(error))
                     
                     console.log("Is doneBtn working?")
